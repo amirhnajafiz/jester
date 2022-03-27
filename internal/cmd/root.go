97105a5c8f01493a062d800371021001cc629154
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-27 20:30:24 +0430

    config added in cmd

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -4,10 +4,12 @@ import (
 	"log"
 
 	"github.com/amirhnajafiz/Stan-Gee/internal/cmd/server"
+	"github.com/amirhnajafiz/Stan-Gee/internal/config"
 )
 
 func Execute() {
-	s, l := server.NewServer(":8080")
+	cfg := config.Load()
+	s, l := server.NewServer(cfg.Server)
 
 	err := s.Serve(l)
 	if err != nil {
