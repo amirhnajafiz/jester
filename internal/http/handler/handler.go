0c67934842a173f839e46e2e10caebfc4e948ad3
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-27 20:05:39 +0430

    handler subscribe message added

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"context"
+	"log"
 
 	"github.com/amirhnajafiz/Stan-Gee/proto"
 	"github.com/nats-io/stan.go"
@@ -13,6 +14,18 @@ type Handler struct {
 
 func (h *Handler) Sub(in *proto.Send, stream proto.StanG_SubServer) error {
 	// send messages via nats
+	sub, _ := h.Stan.Subscribe(in.Topic, func(msg *stan.Msg) {
+		err := stream.Send(&proto.Catch{
+			Content: string(msg.Data),
+		})
+		if err != nil {
+			log.Fatalf("faield to subscribe: %v\n", err)
+		}
+	})
+
+	defer func(sub stan.Subscription) {
+		_ = sub.Unsubscribe()
+	}(sub)
 
 	return nil
 }
