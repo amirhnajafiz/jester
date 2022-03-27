2f844dbddd51a3903996610f2e4740ca62758025
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-27 20:45:19 +0430

    config of telemetery implemented

--- internal/telemetry/config.go
+++ internal/telemetry/config.go
@@ -1,1 +1,22 @@
 package telemetry
+
+type Config struct {
+	Trace  Trace  `koanf:"trace"`
+	Metric Metric `koanf:"metric"`
+}
+
+type Trace struct {
+	Enabled bool `koanf:"enabled"`
+	Agent   `koanf:"agent"`
+	Ration  float64 `koanf:"ratio"`
+}
+
+type Agent struct {
+	Host string `koanf:"host"`
+	Port string `koanf:"port"`
+}
+
+type Metric struct {
+	Enabled bool   `koanf:"enabled"`
+	Address string `koanf:"address"`
+}
