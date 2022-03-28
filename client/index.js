02eb1a44af176477c5778ca659c8166ee8c319e3
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-28 18:18:55 +0430

    a bug in proto service reader

--- client/index.js
+++ client/index.js
@@ -1,21 +1,58 @@
-// import packages
-let grpc = require('@grpc/grpc-js');
-let protoLoader = require('@grpc/proto-loader');
+// import grpc package
+const grpc = require('@grpc/grpc-js');
 
 // out proto file path
 let PROTO_PATH = __dirname + '/../proto/service.proto';
 
+// import proto loader
+let protoLoader = require('@grpc/proto-loader');
+
+// grpc loader options
+const options = {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+};
+
 // Suggested options for similarity to existing grpc.load behavior
 let packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
-    {
-        keepCase: true,
-        longs: String,
-        enums: String,
-        defaults: true,
-    }
+    options
 );
 
 // The protoDescriptor object has the full package hierarchy
-let protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
+const stanG = grpc.loadPackageDefinition(packageDefinition).StanG
 
+const address = "localhost:8080";
+const topic = "topic";
+const type = "original";
+
+// create a client
+const client = new stanG(
+    address,
+    grpc.credentials.createInsecure()
+)
+
+// client methods
+client.sub(
+    {
+        topic: topic,
+        type: type
+    },
+    (error, res) => {
+        if (!error) throw error
+        console.log(res)
+    }
+)
+
+client.put(
+    {
+        content: "Test text"
+    },
+    (error, res) => {
+        if (!error) throw error
+        console.log(res)
+    }
+)
