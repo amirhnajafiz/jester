b138fbef5f112b3a6d9af7461a6342846e8db100
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-28 11:49:05 +0430

    proto service added

--- client/index.js
+++ client/index.js
@@ -1,4 +1,22 @@
-let PROTO_PATH = __dirname + '../proto/service.proto';
-let grpc = require('@grpc/grpc-js')
+// import packages
+let grpc = require('@grpc/grpc-js');
+let protoLoader = require('@grpc/proto-loader');
 
-let protoLoader = require('@grpc/proto-loader')
+// out proto file path
+let PROTO_PATH = __dirname + '../proto/service.proto';
+
+// Suggested options for similarity to existing grpc.load behavior
+let packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    }
+);
+
+let protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
+// The protoDescriptor object has the full package hierarchy
+let service = protoDescriptor.constructor;
