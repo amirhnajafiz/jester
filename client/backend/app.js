85ea89609f6f9d7187af68bdb6f32aff549811a9
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-29 14:12:19 +0430

    app refactor with clients

--- client/backend/app.js
+++ client/backend/app.js
@@ -11,9 +11,52 @@ const cli = CreateClient()
 
 // routes
 app.get('/check', (req, res) => {
-    const topic = req.query.get("topic")
+    const topic = "test1/stan";
+    const send = {
+        topic: topic,
+        type: "normal"
+    };
+    let tests = [
+        {
+            id: "1001",
+            topic: topic,
+            content: this.id,
+            status: false
+        },
+        {
+            id: "1002",
+            topic: topic,
+            content: this.id,
+            status: false
+        },
+        {
+            id: "1003",
+            topic: topic,
+            content: this.id,
+            status: false
+        }
+    ]
 
-    res.send({"topic": topic})
+    cli.sub(send, function (err, resp) {
+        if (err)
+            return null
+
+        tests.forEach((test) => {
+            if (test.id === resp.content) {
+                test.status = true
+            }
+        })
+    })
+
+    tests.forEach((test) => {
+        cli.pub(send, function (err, resp) {
+            if (err)
+                console.error(err)
+            console.log(resp)
+        })
+    })
+
+    res.send({"tests": tests})
 })
 
 // starting the application
