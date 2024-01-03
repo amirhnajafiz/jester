571550f5c89cac7edfa18d830fd03bc94484db9e
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-03 13:04:44 +0330

    add: client.go

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,1 +1,30 @@
 package client
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+type Client struct {
+	Host string
+}
+
+func (c Client) SendPost(body []byte) error {
+	req, err := http.NewRequest(http.MethodPost, c.Host, bytes.NewReader(body))
+	if err != nil {
+		return fmt.Errorf("failed to create post request: %w", err)
+	}
+
+	client := &http.Client{}
+	if resp, er := client.Do(req); er != nil {
+		return fmt.Errorf("failed to send http request: %w", err)
+	} else {
+		if resp.StatusCode != http.StatusOK {
+			return errors.New("server response was not OK")
+		}
+	}
+
+	return nil
+}
