33e75817732e09ae194e971446646e43a9aa0cb2
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-03 13:11:52 +0330

    init: metrics

--- internal/telemetry/metrics/metrics.go
+++ internal/telemetry/metrics/metrics.go
@@ -1,4 +1,12 @@
 package metrics
 
 type Metrics struct {
+	NumberOfSubscribers int // per topic gauge
+	NumberOfPublishers  int // per topic gauge
+	NumberOfPublish     int // per topic counter
+	NumberOfConsume     int // per topic counter
+	NumberOfFailures    int // per topic counter
+	FailedConnections   int // counter
+	RetryPerConnection  int // counter
+	Latency             int // per topic histogram
 }
