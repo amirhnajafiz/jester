7f62566dce261b40fddad3a4704630fe1bb2be51
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-04 01:32:10 +0330

    init: http handler

--- internal/port/http/handler.go
+++ internal/port/http/handler.go
@@ -1,1 +1,27 @@
 package http
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Handler struct{}
+
+func (h Handler) healthy(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("ok"))
+}
+
+func (h Handler) cover(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h Handler) Register(port int) error {
+	http.HandleFunc("/healthz", h.healthy)
+	http.HandleFunc("/cover", h.cover)
+
+	log.Println(fmt.Sprintf("http server running on %d...", port))
+
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
