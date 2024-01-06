545249c279b994687fe608cd7733a3359f026094
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-06 09:30:39 +0330

    add: subscriber metrics interface

--- internal/port/subscriber/metrics.go
+++ internal/port/subscriber/metrics.go
@@ -1,1 +1,10 @@
 package subscriber
+
+type Metrics interface {
+	IncreaseConsumers(topic string)
+	DecreaseConsumers(topic string)
+	AddConsume(topic string, msg string)
+	AddFailure(topic string)
+	AddConnectionFailure()
+	AddRetry(number int)
+}
