257b6b6b02e8bf8945eaf0085d55f9850a569803
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-06 09:29:18 +0330

    add: publisher metrics interface

--- internal/port/publisher/metrics.go
+++ internal/port/publisher/metrics.go
@@ -1,1 +1,10 @@
 package publisher
+
+type Metrics interface {
+	IncreasePublishers(topic string)
+	DecreasePublishers(topic string)
+	AddPublish(topic string, msg string)
+	AddFailure(topic string)
+	AddConnectionFailure()
+	AddRetry(number int)
+}
