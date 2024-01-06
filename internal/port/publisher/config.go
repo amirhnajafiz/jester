253fa5fd552c7b296cbd3a6a2b534ba4220aac35
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-06 09:25:54 +0330

    add: publisher signutare

--- internal/port/publisher/config.go
+++ internal/port/publisher/config.go
@@ -1,1 +1,9 @@
 package publisher
+
+import "time"
+
+type Config struct {
+	Host     string
+	Agent    string
+	Interval time.Duration
+}
--- internal/port/publisher/handler.go
+++ internal/port/publisher/handler.go
@@ -1,1 +1,8 @@
 package publisher
+
+import "github.com/amirhnajafiz/jester/internal/client"
+
+type Handler struct {
+	Cfg    Config
+	Client client.Client
+}
