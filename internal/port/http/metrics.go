4005f825fd6132e87ed673297744526b473178c2
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-08 11:01:16 +0330

    add: process metrics handler

--- internal/port/http/metrics.go
+++ internal/port/http/metrics.go
@@ -1,7 +1,18 @@
 package http
 
+import "github.com/amirhnajafiz/jester/pkg"
+
 func (h Handler) processMetrics(req Request) {
 	switch req.Field {
-
+	case pkg.FieldAddPublisher:
+	case pkg.FieldRemovePublisher:
+	case pkg.FieldAddSubscriber:
+	case pkg.FieldRemoveSubscriber:
+	case pkg.FieldPublish:
+	case pkg.FieldConsume:
+	case pkg.FieldFailures:
+	case pkg.FieldFailedConnections:
+	case pkg.FieldRetryPerConnection:
+	case pkg.FieldLatency:
 	}
 }
--- internal/port/http/request.go
+++ internal/port/http/request.go
@@ -3,7 +3,7 @@ package http
 // Request is being sent by publisher and subscriber
 // in order to update metrics.
 type Request struct {
-	Field string  `json:"field"`
+	Field int     `json:"field"`
 	Label string  `json:"label"`
 	Value float64 `json:"value"`
 }
