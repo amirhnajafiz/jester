2511cfdc5ffb66d821e905cd001878d28a86d3a2
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-10 10:42:18 +0330

    add: subscriber cmd main

--- cmd/consumer.go
+++ cmd/consumer.go
@@ -1,13 +1,33 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/amirhnajafiz/jester/internal/config"
+	"github.com/amirhnajafiz/jester/internal/port/subscriber"
 
-type Consumer struct{}
+	"github.com/spf13/cobra"
+)
+
+type Consumer struct {
+	Cfg config.Config
+}
 
 func (c Consumer) Command() *cobra.Command {
 	return nil
 }
 
 func (c Consumer) main() {
+	// open nats connection
+	conn, err := NewNATSConn(c.Cfg.NATS.Host)
+	if err != nil {
+		panic(err)
+	}
 
+	// register handler
+	err = subscriber.New(subscriber.Config{
+		Agent: c.Cfg.HTTP.Agent,
+		Topic: c.Cfg.NATS.Topic,
+	}, conn).Start()
+	if err != nil {
+		panic(err)
+	}
 }
