724b85425669818186977c797a05826da08cfc6d
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-10 09:34:01 +0330

    add: main cfg load

--- main.go
+++ main.go
@@ -4,19 +4,27 @@ import (
 	"log"
 
 	"github.com/amirhnajafiz/jester/cmd"
+	"github.com/amirhnajafiz/jester/internal/config"
 
 	"github.com/spf13/cobra"
 )
 
 func main() {
+	// root command
 	root := &cobra.Command{}
+
+	// load configs
+	cfg := config.Load("config.yml")
 
 	root.AddCommand(
 		cmd.Publisher{}.Command(),
 		cmd.Consumer{}.Command(),
-		cmd.Agent{}.Command(),
+		cmd.Agent{
+			Cfg: cfg,
+		}.Command(),
 	)
 
+	// execute root command
 	if err := root.Execute(); err != nil {
 		log.Fatal(err)
 	}
