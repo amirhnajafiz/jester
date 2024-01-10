ea50e345a0bf1c68bcb3479e50380691d5fe3c8e
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-10 10:12:04 +0330

    add: publisher and subscriber new handlers

--- internal/port/publisher/handler.go
+++ internal/port/publisher/handler.go
@@ -3,11 +3,21 @@ package publisher
 import (
 	"github.com/amirhnajafiz/jester/internal/client"
 
-	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
 )
 
 type Handler struct {
 	Cfg    Config
 	Client client.Client
-	Conn   *nats.Conn
+	Conn   jetstream.JetStream
+}
+
+func New(cfg Config, conn jetstream.JetStream) *Handler {
+	return &Handler{
+		Conn: conn,
+		Cfg:  cfg,
+		Client: client.Client{
+			Host: cfg.Agent,
+		},
+	}
 }
