f17e2a1718e2b938a213c34260d0d50e925ab599
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-11 12:07:38 +0330

    add: latency check by etcd

--- internal/port/http/metrics.go
+++ internal/port/http/metrics.go
@@ -1,6 +1,11 @@
 package http
 
-import "github.com/amirhnajafiz/jester/pkg"
+import (
+	"log"
+	"time"
+
+	"github.com/amirhnajafiz/jester/pkg"
+)
 
 func (h Handler) processMetrics(req Request) {
 	switch req.Field {
@@ -14,8 +19,28 @@ func (h Handler) processMetrics(req Request) {
 		h.Metrics.UpdateNumberOfSubscribers(req.Label, -1)
 	case pkg.FieldPublish:
 		h.Metrics.IncreaseNumberOfPublish(req.Label)
+
+		if err := h.ETCD.Put(req.Label, time.Now().Format(time.DateTime)); err != nil {
+			log.Println(err)
+		}
 	case pkg.FieldConsume:
 		h.Metrics.IncreaseNumberOfConsume(req.Label)
+
+		tmp, err := h.ETCD.Get(req.Label)
+		if err != nil {
+			log.Println(err)
+
+			return
+		}
+
+		date, err := time.Parse(time.DateTime, tmp)
+		if err != nil {
+			log.Println(err)
+
+			return
+		}
+
+		h.Metrics.AddLatency(req.Label, float64(time.Now().Sub(date).Milliseconds()))
 	case pkg.FieldFailures:
 		h.Metrics.IncreaseNumberOfFailures(req.Label)
 	case pkg.FieldFailedConnections:
