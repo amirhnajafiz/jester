129cd043fff5d4d37ddff0e29500c3ffacf61b3b
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-12 10:31:23 +0330

    refactor: nats client error handler

--- internal/client/nats/client.go
+++ internal/client/nats/client.go
@@ -1,6 +1,10 @@
 package nats
 
-import "github.com/nats-io/nats.go"
+import (
+	"github.com/amirhnajafiz/jester/pkg"
+
+	"github.com/nats-io/nats.go"
+)
 
 type Client struct {
 	Host string
@@ -11,13 +15,13 @@ func (c *Client) Connect() error {
 	// connect to nats server
 	nc, err := nats.Connect(c.Host)
 	if err != nil {
-		return err
+		return pkg.WrapError("nats-client", "connection", err)
 	}
 
 	// create jetstream context from nats connection
 	js, err := nc.JetStream(nil)
 	if err != nil {
-		return err
+		return pkg.WrapError("nats-client", "jetstream", err)
 	}
 
 	// keep connection
