7698791372b37460c1c6326cd12c025f417539a4
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-12 10:30:33 +0330

    refactor: http client error handler

--- internal/client/http/client.go
+++ internal/client/http/client.go
@@ -3,8 +3,9 @@ package http
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"net/http"
+
+	"github.com/amirhnajafiz/jester/pkg"
 )
 
 type Client struct {
@@ -14,15 +15,15 @@ type Client struct {
 func (c Client) SendPost(body []byte) error {
 	req, err := http.NewRequest(http.MethodPost, c.Host, bytes.NewReader(body))
 	if err != nil {
-		return fmt.Errorf("failed to create post request: %w", err)
+		return pkg.WrapError("http-client", "failed to create post request", err)
 	}
 
 	client := &http.Client{}
 	if resp, er := client.Do(req); er != nil {
-		return fmt.Errorf("failed to send http request: %w", err)
+		return pkg.WrapError("http-client", "failed to send post request", er)
 	} else {
 		if resp.StatusCode != http.StatusOK {
-			return errors.New("server response was not OK")
+			return pkg.WrapError("http-client", "server response", errors.New("server response was not OK"))
 		}
 	}
 
